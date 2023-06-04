96fb9107e6330908e08e7c319f4945f33f4ba9fd
AlmohannadAli <135534837+AlmohannadAli@users.noreply.github.com>
2023-06-04 21:47:33 +0300

    Add files via upload

---
+++ main1.dart
@@ -0,0 +1,16 @@
+void main(){
+   dynamic y="almohannad";
+
+    y=40;
+
+    y="ali";
+
+    y=5.2;
+    print(y);
+    print(y.runtimeType.toString());
+
+    var x ="40";
+    
+    print(x.runtimeType.toString());
+
+}  
