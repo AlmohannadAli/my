9aad7f11c157df7ad3a0cdca1d96e0180eda1a3f
AlmohannadAli <135534837+AlmohannadAli@users.noreply.github.com>
2023-06-04 23:17:24 +0300

    Add files via upload

---
+++ main3.dart
@@ -0,0 +1,25 @@
+void main(){
+   dynamic x= sum(50,40);
+
+  int result =sum(20,90);
+  print(result);
+
+  print(x);
+ 
+  print(x.runtimeType.toString());
+
+  String username ="ali";
+  String username2 =username.length.isEven.toString();
+  String usernam2 ="false";
+  print(username2);
+
+}
+ int sum(int firstnmber,int secondnumber){
+ 
+  print(firstnmber+secondnumber);
+
+  var result=firstnmber*secondnumber / 2;
+print(result);
+return 0;
+
+}  
