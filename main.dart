c40b1e08ef7068725735d1dc34701cef1a355eda
AlmohannadAli <135534837+AlmohannadAli@users.noreply.github.com>
2023-06-06 20:56:46 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,17 @@
+void main() {
+int number = 4;                                                  
+    double precent = 90.5;                                             
+    String name = ("almohannad");                                         
+    String lastname ="4";                                                   
+    bool isuser = false;                                             
+
+    dynamic varibale =50;                                              
+    
+
+    print(number);
+    print(precent); 
+    print(name);
+    print(lastname);
+    print(isuser);
+    print(varibale);
+}
